a2ff5c991f6ce782a55976b4440df38a642aade6
Tolulope Awode <54535768+TolulopeAwode@users.noreply.github.com>
2020-04-17 05:01:39 +0100

    first commit

---
+++ GeneralPublisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using CollegeBox.Contract.Commands;
+using CollegeBox.Contract.Config;
+using CollegeBox.Contract.Queries;
+using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeneralPublisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("CollegeBox Event Sourcing>>>>>>");
+            while (true)
+            {
+                Console.Write("Award Description: ");
+                var awrdName = Console.ReadLine();
+                Console.Write("Acronyms: ");
+                var acronym = Console.ReadLine();
+
+                var bus = BusConfigurator.ConfigureBus();
+                Console.Title = "Publisher...";
+                await bus.StartAsync();
+                //await bus.Publish<AwardCommand>(new
+                //{
+                //    Description = awrdName,
+                //    Acronym=acronym,
+                //    InstitutionId=1
+                //});
+
+                var serviceAddress = new Uri($"{RabbitMqConstants.RabbitMqUri}/{RabbitMqConstants.UpdateAward}");
+                var requestTimeout = TimeSpan.FromSeconds(60);
+                var client = bus.CreateRequestClient<EditAwardCommand>(serviceAddress, requestTimeout);
+                var responseContext = await client.GetResponse<GenericResponse<bool>>(new { Id = 1, Description=awrdName, Acronym=acronym, InstitutionId=1 }, CancellationToken.None);
+                if (responseContext.Message.Response)
+                    Console.WriteLine($"Success:>> {responseContext.Message.Message}");
+                else
+                    Console.WriteLine($"Error:>>{responseContext.Message.Message}");
+
+
+
+                Console.WriteLine("Press X to terminate");
+                if (Console.ReadLine().ToLower() == "x") break;
+                await bus.StopAsync();
+                Console.Clear();
+            }
+        }
+    }
+}
